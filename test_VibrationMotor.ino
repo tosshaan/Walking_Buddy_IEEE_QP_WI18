f3b5740d256951b6bcd47f1b0d53bdd423ab17fe
Tosshaan Thapar <31937682+tosshaan@users.noreply.github.com>
2018-02-10 11:51:16 -0800

    Add files via upload
    tests for range sensor and vibration motor

---
+++ test_VibrationMotor.ino
@@ -0,0 +1,16 @@
+int motor = 8;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(motor, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(motor, HIGH);
+  delay(100);
+  digitalWrite(motor, LOW);
+  delay(100);
+
+}
